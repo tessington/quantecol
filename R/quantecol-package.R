5f7fa57abde99978a59e6a11878c9ea7286549fe
Tim Essington <tessington@users.noreply.github.com>
2021-06-10 15:16:19 -0700

    add new package documentation

---
+++ R/quantecol-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @details Example R code to accompany worked examples and figures shown in Introduction to Quantitative Ecology. This includes all of the datasets used, and many of the functions are named based on the figure number within the book text.  You may find it useful to view the code within the functions, which you can see by using the "print" function.  For instance, if you wish to view to code that was used to generate figure 2.2, you can enter print(fig2.2) into the console.
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
